1635757251ce6b04e702028a7a71cae1af325501
ritesh81269@gmail.com <ritesh81269@gmail.com>
2023-10-24 22:01:40 +0530

    401 issue fix

--- backend/utils/sendCookie.js
+++ backend/utils/sendCookie.js
@@ -5,7 +5,9 @@ const sendCookie = (user = {}, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true
+        httpOnly: true,
+        sameSite: 'None',
+        secure: true,  //secure is mandatory for enabling sameSite none attribute
     }
 
     res.status(statusCode).cookie('token', token, options).json({
